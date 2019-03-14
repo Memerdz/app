3f7e63aa919ccbc27343dec28473d74dd5e2be4c
Meme <35967669+Memerdz@users.noreply.github.com>
2019-03-14 09:11:04 -0600

    Add files via upload

---
+++ App/array.js
@@ -0,0 +1,138 @@
+
+    var iphones = [
+        {
+            nombre: "iPhone 6S",
+            color: "Gris",
+            color2: "Espacial",
+            almacenamiento: "64",
+            precio: " 6,999",
+            nomenclatura: "MXN",
+            imagen: "images/iphone-6s.png",
+        },
+        {
+            nombre: "iPhone 7",
+            color: "Gris",
+            color2: "Espacial",
+            almacenamiento: "128",
+            precio: " 7,599",
+            nomenclatura: "MXN",
+            imagen: "images/iphone7.png",
+        },
+        {
+            nombre: "iPhone 7 Plus",
+            color: "Plata",
+            color2: "",
+            almacenamiento: "128",
+            precio: " 9,699",
+            nomenclatura: "MXN",
+            imagen: "images/iphone7-plus.png",
+        },
+        {
+            nombre: "iPhone 8",
+            color: "Gris",
+            color2: "Espacial",
+            almacenamiento: "64",
+            precio: " 12,199",
+            nomenclatura: "MXN",
+            imagen: "images/iphone8.png",
+        },
+        {
+            nombre: "iPhone X",
+            color: "Gris",
+            color2: "Espacial",
+            almacenamiento: "128",
+            precio: " 22,499",
+            nomenclatura: "MXN",
+            imagen: "images/iphoneX.png",
+        },
+        {
+            nombre: "iPhone XR",
+            color: "Coral",
+            color2: "",
+            almacenamiento: "128",
+            precio: " 24,999",
+            nomenclatura: "MXN",
+            imagen: "images/iphoneXR.png",
+        },
+        {
+            nombre: "iPhone XS",
+            color: "Plata",
+            color2: "",
+            almacenamiento: "256",
+            precio: " 28,899",
+            nomenclatura: "MXN",
+            imagen: "images/iphoneXS.png",
+        },
+        {
+            nombre: "iPhone XS MAX",
+            color: "Plata",
+            color2: "",
+            almacenamiento: "256",
+            precio: " 32,799",
+            nomenclatura: "MXN",
+            imagen: "images/iphoneXSmax.png",
+        }
+    ];
+
+
+    var ipads = [
+        {
+            nombre: "12.9 in. iPad Pro",
+            color: "Gris",
+            color2: "Espacial",
+            almacenamiento: "512",
+            precio: "36,831",
+            nomenclatura: "MXN",
+            imagen: "images/ipadpro.png",
+        },
+        {
+            nombre: "11 in. iPad Pro",
+            color: "Plata",
+            color2: "",
+            almacenamiento: "256",
+            precio: "18,999",
+            nomenclatura: "MXN",
+            imagen: "images/ipadpro11.png",
+        },
+        {
+            nombre: "7.9 in. iPad mini 4",
+            color: "Gris",
+            color2: "Espacial",
+            almacenamiento: "128",
+            precio: "13,099",
+            nomenclatura: "MXN",
+            imagen: "images/ipadmini4.png",
+        }
+    ];
+
+
+
+    var watchs = [
+        {
+            nombre: "Watch Series 4",
+            color: "GPS +",
+            color2: "celular",
+            almacenamiento: "44mm",
+            precio: "9,499",
+            nomenclatura: "MXN",
+            imagen: "images/watchseries4.png",
+        },
+        {
+            nombre: "Watch Series 4 Nike",
+            color: "GPS +",
+            color2: "celular",
+            almacenamiento: "44mm",
+            precio: "11,999",
+            nomenclatura: "MXN",
+            imagen: "images/watchseries4nike.png",
+        },
+        {
+            nombre: "Watch Series 4 Hermes",
+            color: "GPS +",
+            color2: "celular",
+            almacenamiento: "40mm",
+            precio: "31,699",
+            nomenclatura: "MXN",
+            imagen: "images/watchseries4hermes.png",
+        }
+    ];
